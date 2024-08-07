6a2337c017003ac7151afa56ec4f1fd24aeb3a22
IiLuha <elyamin@bk.ru>
2024-08-07 20:54:38 +0300

    Created database

---
+++ src/main/java/com/itdev/database/entity/TaskExecutor.java
@@ -0,0 +1,47 @@
+package com.itdev.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(exclude = "user")
+@ToString(exclude = "user")
+@Entity
+@Table(name = "executor")
+public class TaskExecutor implements BaseEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "taskExecutor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        task.setTaskExecutor(this);
+    }
+}
---
+++ src/main/java/com/itdev/database/entity/User.java
@@ -0,0 +1,54 @@
+package com.itdev.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "email")
+@ToString(exclude = {"taskAuthor", "taskExecutor"})
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = {"email"}))
+public class User implements BaseEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private TaskAuthor taskAuthor;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private TaskExecutor taskExecutor;
+
+    public void addAuthor(@NonNull TaskAuthor taskAuthor) {
+        this.taskAuthor = taskAuthor;
+        taskAuthor.setUser(this);
+    }
+
+    public void addExecutor(@NonNull TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+        taskExecutor.setUser(this);
+    }
+}
