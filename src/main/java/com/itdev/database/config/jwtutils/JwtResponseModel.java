59416e8b005472636db5963351a5cb816fcca524
IiLuha <elyamin@bk.ru>
2024-08-14 12:42:29 +0300

    Added JWT security.

---
+++ src/main/java/com/itdev/database/config/jwtutils/JwtResponseModel.java
@@ -0,0 +1,18 @@
+package com.itdev.database.config.jwtutils;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class JwtResponseModel  implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final String token;
+
+    public JwtResponseModel(String token) {
+        this.token = token;
+    }
+    public String getToken() {
+        return token;
+    }
+}
